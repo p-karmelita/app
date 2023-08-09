fcb31221e31516ca5ed265e6aacf664617dde468
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-08-09 22:50:35 +0200

    test for wait for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -80,6 +82,9 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
+        'TEST': {
+                    'NAME': 'mytestdatabase',
+                },
     }
 }
 
