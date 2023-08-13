37c0ba2860771b1d4af7bf3c8331abfb47b37cd6
karpiotr90 <92075179+karpiotr90@users.noreply.github.com>
2023-08-13 10:44:53 +0200

    implement tag listing API

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -18,7 +18,7 @@ TAGS_URL = reverse('recipe:tag-list')
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user."""
-    return get_user_model().objects.create_user(email=email, paassword=password)
+    return get_user_model().objects.create_user(email=email, password=password)
 
 
 class PublicTagsApiTests(TestCase):
